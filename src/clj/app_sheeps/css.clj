5c2596a759216b477ebf6986bf294723ddf5ba04
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-25 15:45:48 -0500

    configuring ui toolkits

--- src/clj/app_sheeps/css.clj
+++ src/clj/app_sheeps/css.clj
@@ -1,6 +1,9 @@
 (ns app-sheeps.css
-  (:require [garden.def :refer [defstyles]]))
+  (:require [garden.def :refer [defstyles]]
+            [garden.core :refer [css]]))
 
 (defstyles screen
-  [:body {:color "red"}]
-)
+  [:body {:color "red"}])
+
+(def h1
+  (css [:h1 {:font-size "11px" :text "green"}]))
