1b90746812ad51dc611396f03ac67a69052e798c
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-27 11:22:22 -0500

    i know this structure well now

---
+++ src/cljs/app_sheeps/pages/post.cljs
@@ -0,0 +1,5 @@
+(ns app-sheeps.pages.post
+    (:require [app-sheeps.articles.article-content :refer [content-store]]
+              [app-sheeps.routing :refer [path-for]]
+              [app-sheeps.pages.posts :refer [current-post]]))
+  
---
+++ src/cljs/app_sheeps/pages/posts.cljs
@@ -0,0 +1,4 @@
+(ns app-sheeps.pages.posts
+    (:require 
+     [app-sheeps.articles.article-content :refer [content-store]]
+     [app-sheeps.routing :refer [path-for]]))
