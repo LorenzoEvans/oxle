06bfd45f5d3be46a9b5d793a4c82a4531f4e17c7
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-27 13:13:16 -0500

    trying bg image..no go

--- src/cljs/app_sheeps/pages/homepage.cljs
+++ src/cljs/app_sheeps/pages/homepage.cljs
@@ -4,7 +4,7 @@
 
 (defn home-page []
       (fn []
-       [:div.flex.flex-column.justify-around.w-80-m.flex-column-m.justify-around-m.b--near-black.bw2
+       [:div.flex.flex-column.justify-around.w-80-m.flex-column-m.justify-around-m.b--near-black.bw2.bg-image
         [:h1.flex.flex-column.justify-center.w-100.w-90-m.flex-column-m.bw2.avenir.dark-gray.f1.ba.b--near-black.bw1.shadow-3.f2-m.hover-bg-dark-gray.hover-near-white.hover-animate.hover-near-black.grow.bg-animate.tc.self-center.dn-m "Maybe Sheeps"]
         [:nav.w-75.items-center.flex.flex-row.justify-between.self-center.mt2.mb2
          [:a.h4.link.dim.avenir.no-underline.near-black.ba.bw2.f3.washed-green.bg-dark-gray.w5.flex.items-center.justify-center {:href "https://github.com/LorenzoEvans"} "Github"]
