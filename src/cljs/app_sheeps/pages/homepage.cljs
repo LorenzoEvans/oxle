8e2aaaec81620835d18845d50fc33db498366dc3
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-27 13:58:30 -0500

    ah well, at least it'll work now

--- src/cljs/app_sheeps/pages/homepage.cljs
+++ src/cljs/app_sheeps/pages/homepage.cljs
@@ -4,16 +4,16 @@
 
 (defn home-page []
       (fn []
-       [:div.flex.flex-column.justify-around.w-80-m.flex-column-m.justify-around-m.b--near-black.bw2.bg-image
-        [:h1.flex.flex-column.justify-center.w-100.w-90-m.flex-column-m.bw2.avenir.dark-gray.f1.ba.b--near-black.bw1.shadow-3.f2-m.hover-bg-dark-gray.hover-near-white.hover-animate.hover-near-black.grow.bg-animate.tc.self-center.dn-m "Maybe Sheeps"]
-        [:nav.w-75.items-center.flex.flex-row.justify-between.self-center.mt2.mb2
-         [:a.h4.link.dim.avenir.no-underline.near-black.ba.bw2.f3.washed-green.bg-dark-gray.w5.flex.items-center.justify-center {:href "https://github.com/LorenzoEvans"} "Github"]
-         [:a.h4.link.dim.avenir.no-underline.near-black.ba.bw2.f3.washed-green.bg-dark-gray.w5.flex.items-center.justify-center {:href "https://twitter.com/0xLEDev"} "Twitter"]
-         [:a.h4.link.dim.avenir.no-underline.near-black.ba.bw2.f3.washed-green.bg-dark-gray.w5.flex.items-center.justify-center {:href "#/posts"} "Posts"]
-         [:a.h4.link.dim.avenir.no-underline.near-black.ba.bw2.f3.washed-green.bg-dark-gray.w5.flex.items-center.justify-center {:href "http://www.lorenzoevans.me/"} "Portfolio"]]
-        [:div.flex.flex-column.bg-near-white.br3.bw2.ba.b--near-black.tc.avenir.navy.f4.pa3.lh-copy.fw2.vh-50.self-center.w-75-m.shadow-3.dn-m.justify-between
-         [:div.fw3.bb.bt.bw1.ma2.b--near-black. "A blog dedicated to *Functional Programming, and other niche and weird *tech things of my choosing."]
-         [:div.fw3.bb.bw1.ma2.b--near-black "Yes, the name is from the 'Maybe Not' Rich Hickey talk."]
-         [:div.f4.fw1.ba.bw1.ma2.b--near-black "* Manufactured on equipment that processes OOP: may contain imperative code & mutable state"]
-         [:div.f4.fw1.ba.bw1.ma2.b--near-black " * may contain non-tech related things because this is my blog and my code is law here. "]]]))
+       [:div.flex.flex-column.justify-around.flex-column-m.justify-around-m.b--near-black.bw2.bg-image.vh-100
+        [:h1.flex.flex-column.justify-center.w-100.w-90-m.flex-column-m.bw2.avenir.dark-gray.f1.ba.b--near-black.bw1.shadow-3.f2-m.tc.self-center "Maybe Sheeps"]
+        [:nav.w-100.items-center.flex.flex-row.justify-around.self-center.mt2.mb2.flex-column-m.justify-between-m.h-100-m
+         [:a.h4.grow.avenir.no-underline.near-black.ba.bw2.f3.washed-green.bg-dark-gray.w5.br3.shadow-3.flex.items-center.justify-center {:href "https://github.com/LorenzoEvans"} "Github"]
+         [:a.h4.grow.avenir.no-underline.near-black.ba.bw2.f3.washed-green.bg-dark-gray.w5.br3.shadow-3.flex.items-center.justify-center {:href "https://twitter.com/0xLEDev"} "Twitter"]
+         [:a.h4.grow.avenir.no-underline.near-black.ba.bw2.f3.washed-green.bg-dark-gray.w5.br3.shadow-3.flex.items-center.justify-center {:href "#/posts"} "Posts"]
+         [:a.h4.grow.avenir.no-underline.near-black.ba.bw2.f3.washed-green.bg-dark-gray.w5.br3.shadow-3.flex.items-center.justify-center {:href "http://www.lorenzoevans.me/"} "Portfolio"]]
+        [:div.flex.flex-column.br3.bw2.ba.b--near-black.tc.avenir.near-white.bg-near-black.f4.pa3.lh-copy.fw2.vh-50.self-center.w-75-m.shadow-3.dn-m.justify-between
+         [:div.fw3.bb.bt.bw1.ma2.b--near-white. "A blog dedicated to *Functional Programming, and other niche and weird *tech things of my choosing."]
+         [:div.fw3.bb.bw1.ma2.b--near-white "Yes, the name is from the 'Maybe Not' Rich Hickey talk."]
+         [:div.f4.fw1.ba.bw1.ma2.b--near-white "* Manufactured on equipment that processes OOP: may contain imperative code & mutable state"]
+         [:div.f4.fw1.ba.bw1.ma2.b--near-white " * may contain non-tech related things because this is my blog and my code is law here. "]]]))
   
--- src/cljs/app_sheeps/pages/posts.cljs
+++ src/cljs/app_sheeps/pages/posts.cljs
@@ -6,10 +6,10 @@
 
 (defn posts-page []
     (fn []
-        [:span.w-75.flex.flex-column.justify-evenly.overflow-scroll.content-center.items-center.bw2.b--black
-            [:h1.w5.tc.avenir "Maybe Posts"
+        [:span.w-100.flex.flex-column.justify-center.overflow-scroll.content-center.items-center.bw2.b--black.bg-image-2.vh-100
+            [:h1.w-100.tc.avenir.bw2.avenir.moon-gray.f1.b--near-black.bw1.f2-m.tc.self-center "Maybe Posts"
                 (for [item content-list]
                     (let [kw (first item) {:keys [title url prev]} (second item)]
                             ^{:key kw}
-                            [:div.w-100.flex.flex-row.justify-center.ma2
-                             [:a.avenir.link.dim.navy.truncate {:href url} title]]))]]))
+                            [:div.w-100.flex.flex-row.justify-center.ma2.shadow-3.ba.bw2.f3.bg-near-black
+                             [:a.avenir.link.dim.near-white.truncate {:href url} title]]))]]))
