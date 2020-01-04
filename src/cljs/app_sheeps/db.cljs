efbcd0d58f8faff47580e0609353640a83056973
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-01-04 16:42:43 -0500

    starting datascript db setup

--- src/cljs/app_sheeps/db.cljs
+++ src/cljs/app_sheeps/db.cljs
@@ -1,4 +1,13 @@
-(ns app-sheeps.db)
+(ns app-sheeps.db
+  (:require [datascript.core :as d]))
 
 (def default-db
   {:name "re-frame"})
+
+
+(def schema {:article/title {:db/type :db.type/string}
+             :article/content {:db/type :db.type/string}
+             :article/tags {:db/cardinality :db.cardinality/many}
+             :article/comments {:db/cardinality :db.cardinality/many}})
+  
+(def conn (d/create-conn schema))
