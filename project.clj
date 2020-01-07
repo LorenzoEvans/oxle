cdfa46afae09ae940442940348cda9a17095c632
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-01-07 12:17:49 -0500

    gah, deploying cljs to heroku is always where things get aggravating

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject app-sheeps "0.1.0-SNAPSHOT"
+(defproject app-sheeps
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.597"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
@@ -50,12 +50,14 @@
                             ["shell" "karma" "start" "--single-run" "--reporters" "junit,dots"]]}
 
   :profiles
-  {:dev
-   {:dependencies [[binaryage/devtools "0.9.11"]
-                   [day8.re-frame/re-frame-10x "0.4.4"]
-                   [day8.re-frame/tracing "0.5.3"]]}
+  {:dev {:dependencies [[binaryage/devtools "0.9.11"]
+                        [day8.re-frame/re-frame-10x "0.4.4"]
+                        [day8.re-frame/tracing "0.5.3"]]}
 
    :prod { :dependencies [[day8.re-frame/tracing-stubs "0.5.3"]]}
    
-   :uberjar {:main app-sheeps.core}
-   })
+   :uberjar {:main app-sheeps.core
+             :aot :all
+             :dependencies [[day8.re-frame/tracing "0.5.3"]
+                            [binaryage/devtools "0.9.11"]
+                            [day8.re-frame/re-frame-10x "0.4.4"]]}})
