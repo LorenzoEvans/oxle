5cd9eaa5db1480dda64f1d95309c6290ae353372
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-01-06 21:31:34 -0500

    I love clojure lol

--- src/cljs/app_sheeps/pages/misc.cljs
+++ src/cljs/app_sheeps/pages/misc.cljs
@@ -1,13 +1,22 @@
 (ns app-sheeps.pages.misc
   (:require [app-sheeps.pages.misc-content :refer [misc-items]]))
 
+
+; we need an array of colors
+; we need to randomly grab an index
+(def colors ["bg-washed-green" "bg-washed-blue" "bg-near-white" "bg-washed-red" "bg-washed-yellow"])
+(def c-count (count colors))
+(def r-idx (rand-int (+ c-count 1)))
+(def color-choice (get colors r-idx))
+(js/console.log color-choice)
 (defn misc-page []
-    [:div.vh-100.flex.flex-row.flex-wrap.
-     (for [item misc-items]
-      [:div.ba.bw1.h-25.w-auto.ma1
-       [:section.f2.avenir (item :title)]
-       [:section (item :type)]
-       [:section [:a {:href (item :link)} (item :title)]]
-       [:div "Tags"
-        (for [tag (:tags item)]
-          [:section [:span.ma1 tag]])]])])
+    [:main.cf.pa2
+     [:div.vh-100.flex.flex-row.flex-wrap.ph2.ma1 
+      (for [item misc-items]
+       [:div.ba.bw1.h-30.w-auto.pv2.grow.dib.ma1 {:class color-choice}
+        [:section.f2.avenir (item :title)]
+        [:section.pa2 (item :type)]
+        [:section.pa2 [:a {:href (item :link)} (item :title)]]
+        [:blockquote.pa2.f2.ba.bw1 "Tags"
+         (for [tag (:tags item)]
+          [:span.ma1.f4 tag])]])]])
--- src/cljs/app_sheeps/pages/misc_content.cljs
+++ src/cljs/app_sheeps/pages/misc_content.cljs
@@ -2,16 +2,22 @@
 
 (def misc-items [{:title "Left"
                   :link "www.google" 
-                  :tags #{"application" "minimalist"}}
+                  :tags #{"application" "minimalist"}
+                  :bg "bg-washed-red"}
+                  
                  {:title "Urbit"
                   :link "www.google" 
-                  :tags #{"application" "minimalist" "dec/dist"}}
+                  :tags #{"application" "minimalist" "dec/dist"}
+                  :bg "bg-washed-green"}
                  {:title "Are.na"
                   :link "www.google" 
-                  :tags #{"site" "minimalist" ""}}
+                  :tags #{"site" "minimalist"}
+                  :bg "bg-washed-yellow"}
                  {:title "Lietal"
                   :link "www.google" 
-                  :tags #{"language"}}
+                  :tags #{"language"}
+                  :bg "bg-washed-blue"}
                  {:title "Max Cooper"
                   :link "www.google" 
-                  :tags #{"music" "art" "algo-art" "digi-art"}}])
+                  :tags #{"music" "art" "algo-art" "digi-art"}
+                  :bg "light-gray"}])
